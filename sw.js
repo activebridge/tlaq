e551cc1d190b12ff9c554874bf6080c83fce087e
Storchak Oleksandr <alexsstorchak@gmail.com>
2026-04-02 11:00:26 +0300

    Adds offline page

--- sw.js
+++ sw.js
@@ -6,11 +6,40 @@ layout: null
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/7.3.0/workbox-sw.js');
 
-const { registerRoute, Route } = workbox.routing;
+const { registerRoute } = workbox.routing;
 const { StaleWhileRevalidate, CacheFirst } = workbox.strategies;
 const { ExpirationPlugin } = workbox.expiration;
 const { CacheableResponsePlugin } = workbox.cacheableResponse;
-const { precacheAndRoute } = workbox.precaching;
+
+const OFFLINE_URL = '/offline.html';
+const OFFLINE_CACHE = 'offline-{{ site.time | date: "%Y%m%d%H%M%S" }}';
+const ONE_YEAR = 365 * 24 * 60 * 60;
+const cacheable200 = new CacheableResponsePlugin({ statuses: [200] });
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(OFFLINE_CACHE).then((cache) => cache.addAll([OFFLINE_URL, '/assets/css/application.css', '/assets/js/search.js']))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k.startsWith('offline-') && k !== OFFLINE_CACHE).map((k) => caches.delete(k)))));
+});
+
+registerRoute(
+  ({ request }) => request.mode === 'navigate',
+  async ({ request }) => fetch(request).catch(() => caches.match(OFFLINE_URL, { cacheName: OFFLINE_CACHE }))
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'style' || request.destination === 'script',
+  new StaleWhileRevalidate({ cacheName: 'assets', plugins: [cacheable200] })
+);
+
+registerRoute(
+  ({ request }) => request.destination === 'font',
+  new CacheFirst({ cacheName: 'fonts', plugins: [cacheable200, new ExpirationPlugin({ maxEntries: 10, maxAgeSeconds: ONE_YEAR })] })
+);
 
 // Runtime caching is intentionally disabled for now.
 // Keep the service worker registration to preserve PWA support.
